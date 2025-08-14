9eae0a58fd6a3db65c08218de00ca29988cb003e
Elena05Z <stom841@mail.ru>
2025-08-14 15:36:28 +0300

    Create test_check_text.py

---
+++ tests/tests_hw/test_check_text.py
@@ -0,0 +1,39 @@
+class Component:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def find_element(self, selector):
+        return self.driver.find_element(By.CSS_SELECTOR, selector)
+    
+    def get_text(self):
+        element = self.find_element()
+        return str(element.text)
+
+
+from selenium import webdriver
+import pytest
+from pages.component import Component
+
+@pytest.fixture(scope="module")
+def browser():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+
+def test_footer_text(browser):
+    component = Component(browser)
+    browser.get("https://demoqa.com/")
+    footer_text = component.get_text("#footer-copyright")
+    expected_text = "© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED."
+    assert footer_text.strip() == expected_text, f"Ожидалось '{expected_text}', получено '{footer_text}'"
+
+
+def test_elements_page_text(browser):
+    component = Component(browser)
+    browser.get("https://demoqa.com/")
+    elements_button = browser.find_element(By.LINK_TEXT, "Elements")  # Найти кнопку по её тексту
+    elements_button.click()  # Нажатие кнопки
+    center_text = component.get_text(".main-header")
+    expected_text = "Please select an item from left to start practice."
+    assert center_text.strip() == expected_text, f"Ожидалось '{expected_text}', получено '{center_text}'"
