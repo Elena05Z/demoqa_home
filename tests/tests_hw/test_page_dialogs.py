012140b69abe4a6c8872d86258335397dd134167
Elena05Z <stom841@mail.ru>
2025-08-18 14:14:30 +0300

    Update test_page_dialogs.py

--- tests/tests_hw/test_page_dialogs.py
+++ tests/tests_hw/test_page_dialogs.py
@@ -10,3 +10,20 @@ import pytest
    def test_modal_elements(page):
        page.go_to_modal_dialogs()
        assert page.count_buttons() == 5, f"Expected 5 buttons but found {page.count_buttons()}."
+
+
+   def test_navigation_modal(page):
+       page.go_to_modal_dialogs()
+       page.refresh_page()
+       page.click_home_icon()
+       current_url = page.driver.current_url
+       assert "demoqa.com/" in current_url, f"Current URL is not home page! Current URL: {current_url}"
+       
+       page.navigate_back()
+       page.set_window_size(900, 400)
+       page.navigate_forward()
+       expected_title = "DEMOQA"
+       actual_title = page.driver.title
+       assert expected_title in actual_title, f"Title mismatch! Expected '{expected_title}', got '{actual_title}'"
+
+       page.set_window_size(1000, 1000)
