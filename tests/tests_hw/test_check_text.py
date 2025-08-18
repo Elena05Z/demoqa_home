14a0006115e4f188b8b8887f62a93285ce83deb3
Elena05Z <stom841@mail.ru>
2025-08-18 13:44:49 +0300

    Update test_check_text.py

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -1,15 +1,3 @@
-class Component:
-    def __init__(self, driver):
-        self.driver = driver
-
-    def find_element(self, selector):
-        return self.driver.find_element(By.CSS_SELECTOR, selector)
-    
-    def get_text(self):
-        element = self.find_element()
-        return str(element.text)
-
-
 from selenium import webdriver
 import pytest
 from pages.component import Component
