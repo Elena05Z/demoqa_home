98656368e9fae58f51bd7a17a459931e8e3c1330
Elena05Z <stom841@mail.ru>
2025-08-19 09:30:58 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,14 +1,18 @@
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage:
-    def __init__(self, driver):
+    def __init__(self, driver, base_url='https://www.saucedemo.com'):
         self.driver = driver
-        self.base_url = 'https://www.saucedemo.com/'
+        self.base_url = base_url
+
     
-    
-  def visit(self):
+    def visit(self):
         return self.driver.get(self.base_url)
+
     
-    
-  def find_element(self, locator):
-        return self.driver.find_element(By.CSS_SELECTOR, locator)
+    def find_element(self, locator):
+        try:
+            return self.driver.find_element(By.CSS_SELECTOR, locator)
+        except NoSuchElementException:
+            raise Exception(f'Элемент {locator} не найден!')
