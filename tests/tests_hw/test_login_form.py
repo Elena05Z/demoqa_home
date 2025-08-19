231404e1d155776193eb4ac3f3eca6cc286fd1da
Elena05Z <stom841@mail.ru>
2025-08-19 10:18:52 +0300

    Create test_login_form.py

---
+++ tests/tests_hw/test_login_form.py
@@ -0,0 +1,17 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+@pytest.fixture(scope="module")
+def browser():
+    driver = webdriver.Chrome()  
+    yield driver
+    driver.quit()
+
+
+def test_select_state_and_city(browser):
+    browser.get("https://demoqa.com/automation-practice-form")
+    state_dropdown = Select(browser.find_element(By.ID, "state"))
+    state_dropdown.select_...
