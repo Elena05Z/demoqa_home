cd53259a0293901e46b712d3b934def945656d40
Elena05Z <stom841@mail.ru>
2025-08-19 09:32:38 +0300

    Update swag_labs.py

--- pages/swag_labs.py
+++ pages/swag_labs.py
@@ -4,7 +4,23 @@ from selenium.common.exceptions import NoSuchElementException
 class SwagLabs(BasePage):
     def exist_icon(self):
         try:
-            self.find_element(locator=(By.CSS_SELECTOR, 'div.login_logo'))
+            self.find_element('div.login_logo')
+            return True
+        except NoSuchElementException:
+            return False
+
+    
+    def exist_username_field(self):
+       try:
+            self.find_element('#user-name')
+            return True
+        except NoSuchElementException:
+            return False
+
+    
+    def exist_password_field(self):
+        try:
+            self.find_element('#password')
             return True
         except NoSuchElementException:
             return False
